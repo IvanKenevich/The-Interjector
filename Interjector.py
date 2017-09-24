a30b7378e9c376bc8678567a61393ebd65a62e5e
IvanKenevich <roranmolotoboyez@gmail.com>
2017-09-24 13:51:05 -0700

    Got the automatic posting working

--- Interjector.py
+++ Interjector.py
@@ -47,16 +47,27 @@ def text_matches(text):
 with requests.session() as s:
     BOARD_NAME = "s"
 
-    thread_catalog = make_json("https://2ch.hk/" + BOARD_NAME + "/threads.json")["threads"]
-    print("There are currently", len(thread_catalog), "threads on /" + BOARD_NAME)
-    i = 0
-    for t in thread_catalog:
-        print("=== SEARCHING THROUGH THREAD " + str(i + 1) + "/" + str(len(thread_catalog)) + " ===")
-        message_numbers = search_thread_for_linux(t)
-        for message_number in message_numbers:
-            print("https://2ch.hk/" + BOARD_NAME + "/res/" + t["num"] + ".html#" + message_number)
-            image_id = make_json("https://2ch.hk/api/captcha/2chaptcha/id?board=" + BOARD_NAME + "&thread=" + t["num"])["id"]
-            image_address = "https://2ch.hk/api/captcha/2chaptcha/image/" + image_id
-        i += 1
+    image_id = make_json("https://2ch.hk/api/captcha/2chaptcha/id?board=" + BOARD_NAME + "&thread=" + "161694010")["id"]
+    image_address = "https://2ch.hk/api/captcha/2chaptcha/image/" + image_id
+
+    captcha_value = input(image_address+"\nEnter value from the captcha above: ")
+
+    print(s.post("https://2ch.hk/makaba/posting.fcgi?json=1",data={"task":"post","board":"b",
+                                                             "thread":"161694010",
+                                                             "captcha_type":"2chaptcha",
+                                                             "comment":"test-roll",
+                                                             "2chaptcha_id":str(image_id),
+                                                             "2chaptcha_value":str(captcha_value)}).json()["Error"])
+    # thread_catalog = make_json("https://2ch.hk/" + BOARD_NAME + "/threads.json")["threads"]
+    # print("There are currently", len(thread_catalog), "threads on /" + BOARD_NAME)
+    # i = 0
+    # for t in thread_catalog:
+    #     print("=== SEARCHING THROUGH THREAD " + str(i + 1) + "/" + str(len(thread_catalog)) + " ===")
+    #     message_numbers = search_thread_for_linux(t)
+    #     for message_number in message_numbers:
+    #         print("https://2ch.hk/" + BOARD_NAME + "/res/" + t["num"] + ".html#" + message_number)
+    #         image_id = make_json("https://2ch.hk/api/captcha/2chaptcha/id?board=" + BOARD_NAME + "&thread=" + t["num"])["id"]
+    #         image_address = "https://2ch.hk/api/captcha/2chaptcha/image/" + image_id
+    #     i += 1
 
 s.close()
