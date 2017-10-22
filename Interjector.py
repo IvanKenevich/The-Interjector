694d611fa30753a1b77acf3fa0a5ec2012b334a7
IvanKenevich <roranmolotoboyez@gmail.com>
2017-10-21 18:29:28 -0700

    Implemented awareness of previous responses. Got a few messages deleted by a mod. (HAHA) Chris started talking smack about this project.

--- Interjector.py
+++ Interjector.py
@@ -1,5 +1,8 @@
 import requests
 import sys
+import os.path
+import time
+
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QImage, QPixmap
@@ -9,24 +12,48 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, Q
 
 INTERJECT = """\n I’d just like to interject for a moment. What you’re refering to as Linux, is in fact, GNU/Lnux, \
 or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather \
-another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital 
+another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital
 system components comprising a full OS as defined by POSIX.\
 Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn \
 of events, the version of GNU which is widely used today is often called “Linux”, and many of its users are not aware \
 that it is basically the GNU system, developed by the GNU Project.
 There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the \
-kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel\
- is an essential part of an operating system, but useless by itself; it can only function in the context of a complete \
- operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically \
- GNU with Linux added, or GNU/Linux. All the so-called “Linux” distributions are really distributions of GNU/Linux."""
+kernel: the program in the system that allocates the machine’s resources to the other programs that you run. The kernel \
+is an essential part of an operating system, but useless by itself; it can only function in the context of a complete \
+operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically \
+GNU with Linux added, or GNU/Linux. All the so-called “Linux” distributions are really distributions of GNU/Linux."""
+
+# BAD WORDS:
+w1 = "линукс"
+w2 = "linux"
+w3 = "пинукс"
+w4 = "линупс"
+w5 = "пинупс"
+w6 = "линух"
+
+# GOOD WORDS:
+wn1 = "gnu/linux"
+wn2 = "gnu-linux"
+wn3 = "гну/линукс"
+wn4 = "гну/пинукс"
+wn5 = "гну/линупс"
+wn6 = "гну/пинупс"
+wn7 = "жну/линукс"
+wn8 = "жну/пинукс"
+wn9 = "жну/линупс"
+wn10 = "жну/пинупс"
+wn11 = "archlinux"  # is it?
+
 
 def text_matches(text):
-    w1 = "линукс"
-    w2 = "linux"
-    wn1 = "gnu/linux"
-    wn2 = "gnu-linux"
-    wn3 = "archlinux"
-    if (w1 in text or w2 in text) and wn1 not in text and wn2 not in text and wn3 not in text:
+    # TODO: PLEASE FIND ANOTHER WAY OF DOING THIS - IT'S DUMB AND UGLY
+    if (w1 in text or w2 in text or w3 in text or w4 in text or w5 in text or w6 in text) \
+            and \
+                    wn1 not in text and wn2 not in text and wn3 not in text and wn4 not in text and wn5 not in text \
+            and \
+                    wn6 not in text and wn7 not in text and wn8 not in text and wn9 not in text and wn10 not in text \
+            and \
+                    wn11 not in text:
         return True
     else:
         return False
@@ -46,6 +73,7 @@ class Interjector(QMainWindow):
         self.s = s
         self.BOARD_NAME = "s"
 
+        self.responded = self.load_responded_from_file()
         self.image_id = 0
         self.messages = []
         self.current_message = ()
@@ -99,7 +127,7 @@ class Interjector(QMainWindow):
 
     def search_threads(self):
         thread_catalog = self.make_json("https://2ch.hk/" + self.BOARD_NAME + "/threads.json")["threads"]
-        thread_catalog = thread_catalog[33:40]
+        thread_catalog = thread_catalog[120:140]
         print("There are currently", len(thread_catalog), "threads on /" + self.BOARD_NAME)
         i = 0
 
@@ -107,7 +135,8 @@ class Interjector(QMainWindow):
             print("=== SEARCHING THROUGH THREAD " + str(i + 1) + "/" + str(len(thread_catalog)) + " ===")
             self.search_thread_for_linux(t)
             i += 1
-        print(self.messages)
+            time.sleep(0.5)
+        print(len(self.messages))
 
     def search_thread_for_linux(self, thread):
         thread_number = thread["num"]
@@ -117,17 +146,19 @@ class Interjector(QMainWindow):
 
         # TODO: What if quote AND message contain the word?
         for message in messages:
-            if text_matches(message.text):  # if it's somewhere in the message
-                # (including quotes from previous messages)
-                quotes = message.find_all("span", attrs={"class": "unkfunc"})
-                if len(quotes) > 0:  # if there are any quotes in the message
-                    for quote in quotes:
-                        if text_matches(quote.text):  # check every quote
-                            break
-                    else:  # if not in any of the quotes, must be in the message itself
-                        self.messages.append((thread_number, message["id"][1:]))
-                else:  # if there are no quotes, the word is in the message
-                    self.messages.append((thread_number, message["id"][1:]))
+            message_number_data = (thread_number, message["id"][1:])
+            if message_number_data not in self.responded:
+                if text_matches(message.text):  # if it's somewhere in the message
+                    # (including quotes from previous messages)
+                    quotes = message.find_all("span", attrs={"class": "unkfunc"})
+                    if len(quotes) > 0:  # if there are any quotes in the message
+                        for quote in quotes:
+                            if text_matches(quote.text):  # check every quote
+                                break
+                        else:  # if not in any of the quotes, must be in the message itself
+                            self.messages.append(message_number_data)
+                    else:  # if there are no quotes, the word is in the message
+                        self.messages.append(message_number_data)
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Return or e.key() == Qt.Key_Enter:
@@ -137,20 +168,49 @@ class Interjector(QMainWindow):
         self.s.close()
 
     def submit_user_input(self):
-        print(s.post("https://2ch.hk/makaba/posting.fcgi?json=1",
-                     files={"task": (None, "post"), "board": (None, (self.BOARD_NAME)),
-                            "thread": (None, str(self.current_message[0])),
-                            "captcha_type": (None, "2chaptcha"),
-                            "comment": (None, ">>" + str(self.current_message[1]) + INTERJECT),
-                            "2chaptcha_id": (None, str(self.image_id)),
-                            "2chaptcha_value": (None, str(self.textField.text()))}).json()["Error"])
-        print("https://2ch.hk/" + self.BOARD_NAME + "/res/" + self.current_message[0] + ".html#" + self.current_message[1])
+        post_response = s.post("https://2ch.hk/makaba/posting.fcgi?json=1",
+                               files={"task": (None, "post"),
+                                      "board": (None, self.BOARD_NAME),
+                                      "thread": (None, str(self.current_message[0])),
+                                      "captcha_type": (None, "2chaptcha"),
+                                      "comment": (None, ">>" + str(self.current_message[1]) + INTERJECT),
+                                      "2chaptcha_id": (None, str(self.image_id)),
+                                      "2chaptcha_value": (None, str(self.textField.text()))
+                                      }
+                               )
+
+        if post_response.json()["Error"] is "None":
+            file = open("responded.txt", "a")
+            file.write(str(self.current_message[0]) + " " + str(self.current_message[1]) + "\n")
+            file.close()
+            print("Message posted successfully: " + "https://2ch.hk/" + self.BOARD_NAME + "/res/" +
+                  self.current_message[0] + ".html#" + self.current_message[1])
+
+        else:
+            print("There was a problem posting your message: " + post_response.json()["Reason"])
+
+        self.textField.setText("Processing...")
+        time.sleep(8)
         self.textField.clear()
         if len(self.messages) > 0:
             self.current_message = self.messages.pop()
             self.load_and_display_captcha()
         else:
             print("You've run out of messages. (WOW!)")
+
+    def load_responded_from_file(self):
+        responded = []
+        if os.path.isfile("responded.txt"):  # if the file with responded messages exists
+            file = open("responded.txt", "r")
+            for line in file:
+                line = line[:-1]  # getting rid of newline character
+                responded.append(tuple(line.split(' ')))
+            file.close()
+        else:                                # if it doesn't - create one
+            file = open("responded.txt", "w")
+            file.close()
+
+        return responded
 
 
 if __name__ == "__main__":
